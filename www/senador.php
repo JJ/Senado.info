9a68e22a23d8708b7fad5a5941789d40bd662bd9
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2010-04-18 23:49:26 +0200

    Añadido gráfico a senador, por Rubén

--- www/senador.php
+++ www/senador.php
@@ -104,6 +104,61 @@ $int_senador = executeQuery("select actividad,fase from intervencion_actividades
 				<div class="actividad_senador">
 					
 					<div class="intervenciones">
+					
+							<?php
+
+		include_once("db.php");
+
+		$arr = executeQuery("select fase from intervencion_actividades where persona_id=$id;");
+
+		foreach ($arr as $item)
+		{
+			if (!isset($act[$item['fase']]))
+				$act[$item['fase']]=1;
+			else
+				$act[$item['fase']]++;
+		}
+
+		$chart="";
+		foreach ($act as $p=>$n)
+		{
+			$chart.="\n\t\t['$p', $n],";
+		}
+		$chart=chop($chart,',');
+
+		?>
+
+			<!--Load the AJAX API-->
+			<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+			<script type="text/javascript">
+			
+			  // Load the Visualization API and the piechart package.
+			  google.load('visualization', '1', {'packages':['piechart']});
+			  
+			  // Set a callback to run when the Google Visualization API is loaded.
+			  google.setOnLoadCallback(drawChart);
+			  
+			  // Callback that creates and populates a data table, 
+			  // instantiates the pie chart, passes in the data and
+			  // draws it.
+			  function drawChart() {
+
+			  // Create our data table.
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Senador');
+				data.addColumn('number', 'Intervencion');
+				data.addRows([<?php echo $chart ?>
+				]);
+
+				// Instantiate and draw our chart, passing in some options.
+				var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+				chart.draw(data, {width: 300, height: 300, is3D: true, title: 'Tipo de intervenciones'});
+			  }
+			</script>
+
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div"></div>
+					
 						<h2><span><?php echo $ints_senador ?></span> intervenciones</h2>
 						<ul>
 						<?php
@@ -113,27 +168,30 @@ $int_senador = executeQuery("select actividad,fase from intervencion_actividades
 							}
 						?>
 						</ul>
+
 					</div>
 					
-					<div class="iniciativas">
+					
+					
+<!--					<div class="iniciativas">
 						<h2><span>1.234</span> iniciativas</h2>
-<!--						<ul>
+						<ul>
 							<li><a href="#">Titulo del intervención</a></li>
 							<li><a href="#">Titulo del intervención</a></li>
 							<li><a href="#">Titulo del intervención</a></li>
 							<li><a href="#">Titulo del intervención</a></li>
-						</ul>-->
+						</ul>
 					</div>
 					
 					<div class="cargos">
 						<h2><span>1.234</span> cargos</h2>
-<!--						<ul>
+						<ul>
 							<li><a href="#">Titulo del intervención</a></li>
 							<li><a href="#">Titulo del intervención</a></li>
 							<li><a href="#">Titulo del intervención</a></li>
-							<li><a href="#">Titulo del intervención</a></li>-->
+							<li><a href="#">Titulo del intervención</a></li>
 						</ul>
-					</div>
+					</div>-->
 				
 				</div><!-- .actividad_senador -->
 				
